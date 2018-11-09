adf48c2f292c7b2824a25c3ca2f28900b46de517
Kusalka Silva <kusalkasilva123@gmail.com>
2018-11-09 12:41:16 +0530

    Add files via upload

---
+++ src/restaurantemployeesystem/RestaurantEmployeeSystem.java
@@ -0,0 +1,16 @@
+
+package restaurantemployeesystem;
+
+public class RestaurantEmployeeSystem {
+
+    public static void main(String[] args) {
+        
+        HomePage home = new  HomePage();
+        home.setVisible(true);
+        home.setLocationRelativeTo(null);
+        
+        
+        
+    }
+    
+}
